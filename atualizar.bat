eade4567a9be318680aeef8d77a4e044414e5489
viicsz <victor1749g@gmail.com>
2026-03-22 19:53:57 -0300

    Atualiza 'atualizat.bat'

--- atualizar.bat
+++ atualizar.bat
@@ -1,6 +1,11 @@
 @echo off
 cd /d "%~dp0"
+taskkill /f /im excel.exe >nul 2>&1
+timeout /t 2 /nobreak >nul
+git fetch origin
+git stash push -u
+git reset --hard origin/main
 git pull origin main
 echo.
-echo Atualizado! Pode fechar essa janela.
+echo ATUALIZADO com sucesso! Pode abrir o Excel novamente.
 pause
